61552858458a5a72d824241f2c29a2064ca02885
Scott Carew <103097506+scott-carew@users.noreply.github.com>
2022-08-16 11:00:56 +1200

    remove todo function working

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,7 +4,7 @@ import { RiCloseCircleLine } from 'react-icons/ri'
 import { TiEdit } from 'react-icons/ti'
 import TodoList from './TodoList'
 
-function Todo({ todos, completeTodo }) {
+function Todo({ todos, completeTodo, removeTodo }) {
   const [edit, setEdit] = useState({
     id: null,
     value: '',
@@ -19,8 +19,14 @@ function Todo({ todos, completeTodo }) {
         {todo.text}
       </div>
       <div className="icons">
-        <RiCloseCircleLine />
-        <TiEdit />
+        <RiCloseCircleLine
+          onClick={() => removeTodo(todo.id)}
+          className="delete-icon"
+        />
+        <TiEdit
+          onClick={() => setEdit({ id: todo.id, value: todo.text })}
+          className="edit-icon"
+        />
       </div>
     </div>
   ))
