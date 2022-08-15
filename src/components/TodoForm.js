2151edd758f397f6a7f856230d9fd4c4cf8f960a
Scott Carew <103097506+scott-carew@users.noreply.github.com>
2022-08-16 11:18:35 +1200

    copy over css

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 function TodoForm(props) {
   const [input, setInput] = useState('')
+
+  const inputRef = useRef(null)
+
+  useEffect(() => {
+    //allows you to focus on whatever you input as the ref
+    inputRef.current.focus()
+  })
 
   const handleChange = (e) => {
     setInput(e.target.value)
@@ -27,6 +34,7 @@ function TodoForm(props) {
           name="text"
           className="todo-input"
           onChange={handleChange}
+          ref={inputRef}
         />
         <button className="todo-button">Add todo</button>
       </form>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -16,6 +16,15 @@ function TodoList() {
     setTodos(newTodos)
   }
 
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return
+    }
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    )
+  }
+
   const removeTodo = (id) => {
     const removeArr = [...todos].filter((todo) => todo.id !== id)
     setTodos(removeArr)
@@ -35,7 +44,12 @@ function TodoList() {
     <div>
       <h1>What's the Plan for Today?</h1>
       <TodoForm onSubmit={addTodo} />
-      <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
     </div>
   )
 }
